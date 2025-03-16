03366f25bcd6c530d2248493765aa6b672f0f8c0
msn698 <ms698@uowmail.edu.au>
2025-03-16 21:31:23 +0400

    Update: Remove logo from footer and improve layout

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-[#121212]">
-      <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-center">
+        <p className="text-slate-600">© 2024 Mohammed Saeed. All rights reserved.</p>
       </div>
     </footer>
   );
